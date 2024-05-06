11c2048b167332b7ab3cb8bbbd6f7e9794e4c489
Rees Davison <rees@hazy.com>
2024-05-06 16:16:29 +0100

    Render 2 spheres

--- main.js
+++ main.js
@@ -14,37 +14,24 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// const geometryCube = new THREE.BoxGeometry(1, 1, 1);
-// const materialCube = new THREE.MeshBasicMaterial({
-//   color: 0x00ff00,
-//   opacity: 0.3,
-// });
-// const cube = new THREE.Mesh(geometryCube, materialCube);
-// scene.add(cube);
-
 const light = new THREE.AmbientLight(0x404040, 10); // soft white light
 scene.add(light);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
 scene.add(directionalLight);
 
-const geometrySphere = new THREE.SphereGeometry(); // 15, 32, 16);
-// const materialSphere = new THREE.MeshBasicMaterial({
-//   color: 0xffff00,
-//   wireframe: true,
-// });
-// const materialSphere = new THREE.MeshDepthMaterial({
-//   color: 0xffff00,
-//   // wireframe: true,
-// });
-// const materialSphere = new THREE.MeshNormalMaterial();
-const materialSphere = new THREE.MeshPhongMaterial({
-  color: 0xaf748d,
-  // wireframe: true,
-});
-// const materialSphere = new THREE.WireframeGeometry();
-const sphere = new THREE.Mesh(geometrySphere, materialSphere);
-scene.add(sphere);
+function getSphere(color = 0xaf748d) {
+  const geometrySphere = new THREE.SphereGeometry();
+  const materialSphere = new THREE.MeshPhongMaterial({
+    color: color,
+  });
+  const sphere = new THREE.Mesh(geometrySphere, materialSphere);
+  scene.add(sphere);
+  return sphere;
+}
+
+const sphere1 = getSphere();
+const sphere2 = getSphere(0x4d9140);
 
 camera.position.z = 5;
 
@@ -53,11 +40,13 @@ let time = 0;
 function animate() {
   requestAnimationFrame(animate);
 
-  sphere.position.x = Math.sin(time);
-  sphere.position.y = Math.sin(time);
-  sphere.position.z = Math.sin(time);
-  // cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.01;
+  sphere1.position.x = Math.sin(time);
+  sphere1.position.y = Math.sin(time);
+  sphere1.position.z = Math.sin(time);
+
+  sphere2.position.x = Math.cos(time);
+  sphere2.position.y = Math.cos(time);
+  sphere2.position.z = Math.cos(time);
 
   renderer.render(scene, camera);
   time += 0.01;
