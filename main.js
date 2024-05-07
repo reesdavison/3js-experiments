74765586480a5958f3cb2fd4a02b941c3a7217fe
Rees Davison <rees@hazy.com>
2024-05-07 13:51:23 +0100

    Really basic gravity simulation using 1st order euler method

--- main.js
+++ main.js
@@ -23,7 +23,12 @@ scene.add(light);
 const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
 scene.add(directionalLight);
 
-function getSphere(initPosition = [0, 0, 0], mass = 1, color = 0xaf748d) {
+function getSphere(
+  initPosition = [0, 0, 0],
+  initVelocity = [0, 0, 0],
+  mass = 1,
+  color = 0xaf748d
+) {
   const geometrySphere = new THREE.SphereGeometry(0.25);
   const materialSphere = new THREE.MeshPhongMaterial({
     color: color,
@@ -34,11 +39,12 @@ function getSphere(initPosition = [0, 0, 0], mass = 1, color = 0xaf748d) {
     sphere: sphere,
     mass: mass,
     position: initPosition,
+    velocity: initVelocity,
   };
 }
 
-const sphere1 = getSphere([0, 0, 0], 10 ** 10, 0xaf748d);
-const sphere2 = getSphere([10, 10, 0], 1, 0x446df6);
+const sphere1 = getSphere([0, 0, 0], [0, 0, 0], 10 ** 11, 0xaf748d);
+const sphere2 = getSphere([2, 0, 0], [0, 1, 0], 100, 0x446df6);
 
 function getForce(obj1, obj2) {
   let u_ab0 = obj2.position[0] - obj1.position[0];
@@ -80,19 +86,30 @@ function addArrow(newDir, newOrigin) {
 
 const forceArrow = addArrow([0, 0, 0], [0, 0, 0]);
 
+const TIME_STEP = 0.01;
+
 function animate() {
   requestAnimationFrame(animate);
 
-  sphere1.sphere.position.x = 0; //Math.sin(time);
-  sphere1.sphere.position.y = 0; //Math.sin(time);
-  sphere1.sphere.position.z = 0; //Math.sin(time);
-
-  sphere2.position[0] = radius * Math.cos(time);
-  sphere2.position[1] = radius * Math.sin(time);
-  sphere2.position[2] = Math.cos(time);
+  sphere1.sphere.position.x = 0;
+  sphere1.sphere.position.y = 0;
+  sphere1.sphere.position.z = 0;
 
   const forceOnSphere1 = getForce(sphere1, sphere2);
   const forceOnSphere2 = getForce(sphere2, sphere1);
+
+  const acc = forceOnSphere2.map((force_comp) => force_comp / sphere2.mass);
+  const vel0 = sphere2.velocity[0] + TIME_STEP * acc[0];
+  const vel1 = sphere2.velocity[1] + TIME_STEP * acc[1];
+  const vel2 = sphere2.velocity[2] + TIME_STEP * acc[2];
+
+  const pos0 = sphere2.position[0] + TIME_STEP * vel0;
+  const pos1 = sphere2.position[1] + TIME_STEP * vel1;
+  const pos2 = sphere2.position[2] + TIME_STEP * vel2;
+
+  // update sphere object
+  sphere2.position = [pos0, pos1, pos2];
+  sphere2.velocity = [vel0, vel1, vel2];
 
   console.assert(
     forceOnSphere2[0] + forceOnSphere1[0] < 0.01 &&
@@ -108,13 +125,13 @@ function animate() {
   forceArrow.setLength(
     Math.sqrt(
       forceOnSphere2[0] ** 2 + forceOnSphere2[1] ** 2 + forceOnSphere2[2] ** 2
-    ) * 5
+    ) * 0.01
   );
 
   updatePosition(sphere2);
 
   renderer.render(scene, camera);
-  time += 0.01;
+  time += TIME_STEP;
 }
 
 import WebGL from "three/addons/capabilities/WebGL.js";
