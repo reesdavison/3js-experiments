8021a7000cb0f0eae2460b6df83df470bd91a285
Rees Davison <rees@hazy.com>
2024-05-29 16:49:18 +0100

    Get vite build and vite preview working

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { sync } from "glob";
+
+export default defineConfig({
+  root: resolve(__dirname, "src"),
+  base: "/3js-experiments/",
+  // base: "/",
+  // optimizeDeps: {
+  //   include: ["three"],
+  // },
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    // commonjsOptions: {
+    //   include: ["three", "/node_modules/"],
+    // },
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        multiBall: resolve(__dirname, "src/views/multiBall.html"),
+        multiBall2: resolve(__dirname, "src/views/multiBall2.html"),
+      },
+      // input: {
+      //   main: resolve(__dirname, "src/index.html"),
+      // input: Object.fromEntries(sync(resolve(__dirname, "src/views", "*.html")),
+
+      // },
+      // output: {
+      // format: "es",
+      // dir: "dist",
+      // },
+      // output: {
+      //   main: resolve(__dirname, "dist"),
+      // },
+    },
+  },
+  // resolve: {
+  //   alias: {
+  //     three: 'web3/dist/web3.min.js',
+  //   },
+  // }
+});
